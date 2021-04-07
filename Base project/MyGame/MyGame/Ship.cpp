83919680ed7611a3eff58d2c033d374730bddf07
Elliot Bolstad <bolstade@rsd407.org>
2021-04-07 12:49:16 -0700

    commit3schfinal

--- Base project/MyGame/MyGame/Ship.cpp
+++ Base project/MyGame/MyGame/Ship.cpp
@@ -1,2 +1,12 @@
 #include ""Ship.h"
 
+Ship::Ship()
+{
+	sprite_.setTexture(GAME.getTexture("Resouses/ship.png"));
+	sprite_.setPosition(sf::Vector2f(100, 100));
+}
+
+void Ship::draw()
+{
+	GAME.getRenderWindow().draw(sprite_);
+}
