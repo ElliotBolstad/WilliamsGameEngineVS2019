74c883db37dd81e61c88d19384ac2a74fa4b89e0
Elliot Bolstad <bolstade@rsd407.org>
2021-05-24 12:43:14 -0700

    sounds

--- Base project/MyGame/MyGame/MyGame.cpp
+++ Base project/MyGame/MyGame/MyGame.cpp
@@ -4,7 +4,7 @@
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
 
-const std::string WINDOW_TITLE = "Elliot's Game";
+const std::string WINDOW_TITLE = "Elliot's Meteor Blaster";
 
 int main()
 {
